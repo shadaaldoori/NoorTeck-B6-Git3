5df1f6990756c9835372c2d7f0813090d3254fa4
shada <shada@LAPTOP-IRFJU36P.myfiosgateway.com>
2022-05-08 11:08:16 -0400

    new 3...

---
+++ src/main/java/DemoThree.java
@@ -0,0 +1,15 @@
+
+public class DemoThree {
+			
+		public static void main(String[] args) {
+			
+			System.out.println("Git demo 2...");
+			System.out.println("Git demo 3...");
+		}
+
+			
+			public void abstract(int a , int b) {
+				System.out.println(a-b);
+
+}
+}
